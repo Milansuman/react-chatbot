4d4c2d6dccec065bc5e6ca1c894232e58bec57cf
Risk-taker0501 <risk.taker2024@gmail.com>
2023-09-05 20:54:28 +0300

    change button and add more language

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from "react";
 import data from "./steps.json";
-import dataSpanish from "./steps-spanish.json";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Chatbot from "./components/chatbot";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route
-          exact
-          path="/react-chatbot/1"
-          element={<Chatbot steps={data.steps} name={1} />}
-        />
-        <Route
-          exact
-          path="/react-chatbot/2"
-          element={<Chatbot steps={dataSpanish.steps} name={2} />}
-        />
-      </Routes>
-    </Router>
-  );
+  return <Chatbot allSteps={data} name={1} />;
 };
 
 export default App;
--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -73,16 +73,36 @@ const Review = props => {
   );
 };
 
-const Chatbot = ({ steps, name }) => {
+const Chatbot = ({ allSteps, name }) => {
   if (!localStorage.getItem("clinic" + name))
     localStorage.setItem("clinic" + name, JSON.stringify([]));
-
+  const [isOpened, setIsOpened] = useState(false);
   const [showChatbot, setShowChatbot] = useState(false);
   const [profiles, setProfiles] = useState(
     localStorage.getItem("clinic" + name)
       ? JSON.parse(localStorage.getItem("clinic" + name))
       : []
   );
+  const [key, setKey] = useState(0);
+  const [steps, setSteps] = useState([
+    {
+      id: "1",
+      message: "Please choose your language.",
+      trigger: "lang"
+    },
+    {
+      id: "lang",
+      options: [
+        { value: "en", label: "English", end: true },
+        { value: "sp", label: "Español", end: true },
+        { value: "pt", label: "Português", end: true },
+        { value: "ru", label: "Русский", end: true },
+        { value: "ch", label: "中文", end: true },
+        { value: "ja", label: "日本語", end: true },
+        { value: "hi", label: "हिंदी", end: true }
+      ]
+    }
+  ]);
 
   const handleNameValidation = value => {
     // Name validation logic
@@ -118,7 +138,7 @@ const Chatbot = ({ steps, name }) => {
     return true;
   };
 
-  const newSteps = steps.map(child => {
+  const newSteps = steps?.map(child => {
     if (child.id === "enter-name" || child.id === "question-enter-name") {
       return {
         ...child,
@@ -154,53 +174,167 @@ const Chatbot = ({ steps, name }) => {
     return child;
   });
 
+  const returnState = () => {
+    setSteps([
+      {
+        id: "1",
+        message: "Please choose your language.",
+        trigger: "lang"
+      },
+      {
+        id: "lang",
+        options: [
+          { value: "en", label: "English", end: true },
+          { value: "sp", label: "Español", end: true },
+          { value: "pt", label: "Português", end: true },
+          { value: "ru", label: "Русский", end: true },
+          { value: "ch", label: "中文", end: true },
+          { value: "ja", label: "日本語", end: true },
+          { value: "hi", label: "हिंदी", end: true }
+        ]
+      }
+    ]);
+    setKey(0);
+  };
+
   const handleClick = () => {
     setShowChatbot(!showChatbot);
+    setIsOpened(true);
   };
+
   let tmp = {
     name: "",
     email: "",
     phone: "",
     day: ""
   };
+
   const handleEnd = value => {
-    const values = value.renderedSteps;
-    values.forEach(element => {
-      switch (element.id) {
-        case "enter-name":
-          tmp.name = element.value;
-          break;
-        case "enter-phone-number":
-          tmp.phone = element.value;
-          break;
-        case "enter-email":
-          tmp.email = element.value;
-          break;
-        case "choose-day":
-          tmp.day = element.value;
-          let tt = JSON.parse(localStorage.getItem("clinic" + name));
-          tt.push(tmp);
-          localStorage.setItem("clinic" + name, JSON.stringify(tt));
-          setProfiles(tt);
-          break;
-        case "question-enter-name":
-          tmp.name = element.value;
-          break;
-        case "question-enter-email":
-          tmp.email = element.value;
-          tt = JSON.parse(localStorage.getItem("clinic" + name));
-          tt.push(tmp);
-          localStorage.setItem("clinic" + name, JSON.stringify(tt));
-          setProfiles(tt);
-          break;
-        case "question-enter-phone-number":
-          tmp.phone = element.value;
-          break;
-        default:
-          break;
-      }
-    });
+    const vals = value.steps;
+    if (vals.lang) {
+      setTimeout(() => {
+        setSteps(allSteps[`steps-${vals.lang.value}`]);
+        setKey(1);
+        return true;
+      }, 1000);
+    } else {
+      const values = value.renderedSteps;
+      values.forEach(element => {
+        switch (element.id) {
+          case "enter-name":
+            tmp.name = element.value;
+            break;
+          case "enter-phone-number":
+            tmp.phone = element.value;
+            break;
+          case "enter-email":
+            tmp.email = element.value;
+            break;
+          case "choose-day":
+            tmp.day = element.value;
+            let tt = JSON.parse(localStorage.getItem("clinic" + name));
+            tt.push(tmp);
+            localStorage.setItem("clinic" + name, JSON.stringify(tt));
+            setProfiles(tt);
+            break;
+          case "question-enter-name":
+            tmp.name = element.value;
+            break;
+          case "question-enter-email":
+            tmp.email = element.value;
+            tt = JSON.parse(localStorage.getItem("clinic" + name));
+            tt.push(tmp);
+            localStorage.setItem("clinic" + name, JSON.stringify(tt));
+            setProfiles(tt);
+            break;
+          case "question-enter-phone-number":
+            tmp.phone = element.value;
+            break;
+          default:
+            break;
+        }
+      });
+    }
   };
+
+  const BotButtonWrapper = styled.div`
+    background-color: #0080ff !important;
+    padding: 0;
+    margin: 0;
+    width: 86px;
+    height: 58px;
+    position: fixed;
+    z-index: 10;
+    right: 0;
+    bottom: 38px;
+    display: ${showChatbot ? "none" : "block"};
+    border-radius: 28px 0 0 28px !important;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2), 0 6px 16px rgba(0, 0, 0, 0.1) !important;
+    cursor: pointer;
+
+    &:hover {
+      & div {
+        visibility: visible !important;
+        opacity: 1;
+      }
+    }
+
+    &:before {
+      height: 50px;
+      width: 50px;
+      content: " ";
+      background: url("/react-chatbot/unnamed.jpg") center center/cover no-repeat;
+      border-radius: 25px;
+      position: absolute;
+      z-index: 11;
+      top: 50%;
+      transform: translate3d(5px, -50%, 0);
+      box-shadow: 0 0 4px rgba(0, 0, 0, 0.1), 0 2px 4px rgba(0, 0, 0, 0.2) !important;
+    }
+
+    & div {
+      visibility: hidden !important;
+      position: absolute;
+      background-color: #fff;
+      border-radius: 5px 0 0 28px !important;
+      max-width: 380px !important;
+      max-height: 200px !important;
+      padding: 10px 40px 10px 10px !important;
+      cursor: default;
+      box-shadow: 0 3px 12px rgba(0, 0, 0, 0.2), 0 6px 20px rgba(0, 0, 0, 0.1) !important;
+      right: 0;
+      bottom: 0;
+      z-index: 10;
+      opacity: 0;
+      width: auto !important;
+      min-height: 58px !important;
+      box-sizing: border-box !important;
+      transform-origin: 50% 50% !important;
+      transition: all 0.5s !important;
+    }
+    & button {
+      color: #fff;
+      font-weight: 500 !important;
+      font-size: 16px;
+      padding: 10px 60px !important;
+      margin: 0;
+      background-color: #0080ff;
+      border: none;
+      border-radius: 100px !important;
+      display: inline;
+      white-space: nowrap !important;
+      cursor: pointer;
+    }
+    & p {
+      margin: 0;
+      text-align: center;
+      font-weight: 700;
+      color: #575757 !important;
+      padding: 0px 10px;
+      margin-bottom: 30px !important;
+      font-size: 15px;
+    }
+  `;
 
   return (
     <React.Fragment>
@@ -239,102 +373,128 @@ const Chatbot = ({ steps, name }) => {
       <Chatbotwrapper
         style={{
           position: "fixed",
-          bottom: "20px",
-          right: "20px",
+          bottom: "38px",
+          right: 0,
           display: "flex",
           flexDirection: "column",
           alignItems: "end"
         }}
       >
-        <ChatBot
-          botDelay={4000}
-          handleEnd={handleEnd}
-          botAvatar="/react-chatbot/unnamed.jpg"
-          bubbleOptionStyle={{
-            fontSize: "22px",
-            marginLeft: "86px",
-            textAlign: "start",
-            background: "#4040ff"
-          }}
-          bubbleStyle={{
-            fontSize: "22px",
-            fontFamily: "Arial",
-            background: "#0080ff"
-          }}
-          contentStyle={{
-            width: "600px",
-            flexGrow: 1,
-            fontFamily: "Arial"
-          }}
-          avatarStyle={{
-            width: "80px",
-            height: "80px"
-          }}
-          inputStyle={{
-            fontSize: "22px"
-          }}
-          headerComponent={
-            <h1
-              style={{
-                textAlign: "center",
-                background: "#0080ff",
-                color: "white",
-                margin: 0,
-                paddingTop: "20px",
-                paddingBottom: "10px"
-              }}
-            >
-              Clinic{name}
-            </h1>
-          }
-          steps={newSteps}
-          style={{
-            display: !showChatbot ? "none" : "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            width: "600px",
-            height: "80vh",
-            whiteSpace: "pre-line",
-            fontFamily: "Helvetica",
-            marginBottom: "20px",
-            transformOrigin: "right bottom",
-            transition:
-              "width 200ms ease 0s, height 200ms ease 0s, max-height 200ms ease 0s, transform 300ms cubic-bezier(0, 1.2, 1, 1) 0s, opacity 83ms ease-out 0s"
-          }}
-        />
+        {isOpened && (
+          <ChatBot
+            key={key}
+            botDelay={4000}
+            handleEnd={handleEnd}
+            botAvatar="/react-chatbot/unnamed.jpg"
+            bubbleOptionStyle={{
+              fontSize: "22px",
+              marginLeft: "86px",
+              textAlign: "start",
+              background: "#4040ff"
+            }}
+            bubbleStyle={{
+              fontSize: "22px",
+              fontFamily: "Arial",
+              background: "#0080ff"
+            }}
+            contentStyle={{
+              width: "600px",
+              flexGrow: 1,
+              fontFamily: "Arial"
+            }}
+            avatarStyle={{
+              width: "80px",
+              height: "80px"
+            }}
+            inputStyle={{
+              fontSize: "22px"
+            }}
+            headerComponent={
+              <h1
+                style={{
+                  textAlign: "center",
+                  background: "#0080ff",
+                  color: "white",
+                  margin: 0,
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  paddingTop: "10px",
+                  paddingBottom: "10px"
+                }}
+              >
+                <span style={{ flexGrow: 1 }}>Clinic{name}</span>
+                <div
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    marginRight: "10px",
+                    cursor: "pointer"
+                  }}
+                  onClick={returnState}
+                >
+                  <svg
+                    fill="#fff"
+                    height="30px"
+                    width="30px"
+                    version="1.1"
+                    id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 489.645 489.645"
+                  >
+                    <g>
+                      <path
+                        d="M460.656,132.911c-58.7-122.1-212.2-166.5-331.8-104.1c-9.4,5.2-13.5,16.6-8.3,27c5.2,9.4,16.6,13.5,27,8.3
+		c99.9-52,227.4-14.9,276.7,86.3c65.4,134.3-19,236.7-87.4,274.6c-93.1,51.7-211.2,17.4-267.6-70.7l69.3,14.5
+		c10.4,2.1,21.8-4.2,23.9-15.6c2.1-10.4-4.2-21.8-15.6-23.9l-122.8-25c-20.6-2-25,16.6-23.9,22.9l15.6,123.8
+		c1,10.4,9.4,17.7,19.8,17.7c12.8,0,20.8-12.5,19.8-23.9l-6-50.5c57.4,70.8,170.3,131.2,307.4,68.2
+		C414.856,432.511,548.256,314.811,460.656,132.911z"
+                      />
+                    </g>
+                  </svg>
+                </div>
+              </h1>
+            }
+            steps={newSteps}
+            style={{
+              marginRight: "20px",
+              display: !showChatbot ? "none" : "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              width: "600px",
+              height: "80vh",
+              whiteSpace: "pre-line",
+              fontFamily: "Helvetica",
+              marginBottom: "20px",
+              transformOrigin: "right bottom",
+              transition:
+                "width 200ms ease 0s, height 200ms ease 0s, max-height 200ms ease 0s, transform 300ms cubic-bezier(0, 1.2, 1, 1) 0s, opacity 83ms ease-out 0s"
+            }}
+          />
+        )}
+        <BotButtonWrapper onClick={handleClick}>
+          <div>
+            <p>
+              <strong>What brings you here today?</strong>
+            </p>
+            <button>CHAT LIVE NOW</button>
+          </div>
+        </BotButtonWrapper>
         <div
           style={{
+            display: !showChatbot ? "none" : "flex",
             background: "#0080ff",
             width: "48px",
             height: "48px",
-            display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            borderRadius: "100%"
+            borderRadius: "100%",
+            cursor: "pointer"
           }}
           onClick={handleClick}
         >
           <div
             style={{
-              display: showChatbot ? "none" : "block",
-              width: "24px",
-              height: "24px"
-            }}
-          >
-            <svg
-              focusable="false"
-              aria-hidden="true"
-              viewBox="0 0 28 32"
-              fill="white"
-              width="24"
-              height="24"
-            >
-              <path d="M28,32 C28,32 23.2863266,30.1450667 19.4727818,28.6592 L3.43749107,28.6592 C1.53921989,28.6592 0,27.0272 0,25.0144 L0,3.6448 C0,1.632 1.53921989,0 3.43749107,0 L24.5615088,0 C26.45978,0 27.9989999,1.632 27.9989999,3.6448 L27.9989999,22.0490667 L28,22.0490667 L28,32 Z M23.8614088,20.0181333 C23.5309223,19.6105242 22.9540812,19.5633836 22.5692242,19.9125333 C22.5392199,19.9392 19.5537934,22.5941333 13.9989999,22.5941333 C8.51321617,22.5941333 5.48178311,19.9584 5.4277754,19.9104 C5.04295119,19.5629428 4.46760991,19.6105095 4.13759108,20.0170667 C3.97913051,20.2124916 3.9004494,20.4673395 3.91904357,20.7249415 C3.93763774,20.9825435 4.05196575,21.2215447 4.23660523,21.3888 C4.37862552,21.5168 7.77411059,24.5386667 13.9989999,24.5386667 C20.2248893,24.5386667 23.6203743,21.5168 23.7623946,21.3888 C23.9467342,21.2215726 24.0608642,20.9827905 24.0794539,20.7254507 C24.0980436,20.4681109 24.0195551,20.2135019 23.8614088,20.0181333 Z"></path>
-            </svg>
-          </div>
-          <div
-            style={{
-              display: !showChatbot ? "none" : "block",
               width: "24px",
               height: "24px"
             }}
