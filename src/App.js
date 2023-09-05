1edeacaba5879c581564415e82d5c518a700b626
Risk-taker0501 <risk.taker2024@gmail.com>
2023-09-05 21:09:24 +0300

    change button and add more language

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import data from "./steps.json";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Chatbot from "./components/chatbot";
 
 const App = () => {
